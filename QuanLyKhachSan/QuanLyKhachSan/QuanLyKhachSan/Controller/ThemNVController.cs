bddf5924bb4b748d13d0705a127b1a6ea79bb9e9
thuylinh <thuylinhnd97@gmail.com>
2018-06-09 17:06:16 +0700

    quản lý đặt phòng

--- QuanLyKhachSan/QuanLyKhachSan/QuanLyKhachSan/Controller/ThemNVController.cs
+++ QuanLyKhachSan/QuanLyKhachSan/QuanLyKhachSan/Controller/ThemNVController.cs
@@ -14,42 +14,42 @@ namespace QuanLyKhachSan.Controller
     {
         string day, month, year;
         Connection conn = new Connection();
-        //public void ThemNhanVien(TextBoxX tbhoten,BunifuDropdown ddGioiTinh, TextBoxX tbpass, TextBoxX tbphone,BunifuDatepicker DPNgaySinh, BunifuDropdown ddchucvu,string btavtar,out int transfer)
-        //{
-        //    transfer = 0;
-        //    HotelObject.NhanVienHo nv = new HotelObject.NhanVienHo(); 
-        //    string temp=null;
-        //    ChuanHoa ch = new ChuanHoa();
-        //    nv.NgaySinh= DPNgaySinh.Value.ToString();
-        //    ch.ChuanHoaDate(nv.NgaySinh, out day, out month, out year);
-        //    nv.NgaySinh = year + month + day;
-        //    nv.MaNhanVien = conn.LayBien("EXEC PROC_SELECT_MANHANVIEN", 0);
-        //    for (int i = 2; i < nv.MaNhanVien.Length; i++)
-        //    {
-        //        temp = temp + nv.MaNhanVien[i];
-        //    }
-        //    nv.MaNhanVien = "NV"+ (Int32.Parse(temp) + 1).ToString();
-        //    nv.HoTen = ch.CH_Name(tbhoten);
-        //    if(ch.Check_Phone(tbphone)==false)
-        //    {
-        //        Notification nf = new Notification("LỖI", "Số điện thoại không hợp lệ", "Mời bạn nhập lại.");
-        //        nf.Show();
-        //        transfer = 0;
-        //    }
-        //    else if(ch.Check_Text_Name(tbhoten)==false)
-        //    {
-        //        Notification nf = new Notification("LỖI", "Tên nhập chứa chữ số", "Mời bạn nhập lại.");
-        //        nf.Show();
-        //        transfer = 0;
-        //    }
-        //    else
-        //    {
-        //        conn.InsertDeleteUpdate("EXEC PROC_INSERT_NHANVIEN '" + nv.MaNhanVien + "','" + tbpass.Text + "',N'" + tbhoten.Text + "',N'" + ddchucvu.selectedValue + "',N'" + ddGioiTinh.selectedValue + "','" + btavtar + "','" + year + month + day + "','" + tbphone.Text + "'");
-        //        Notification nf = new Notification("THÊM NHÂN VIÊN", "Thêm nhân viên thành công.", "Mã nhân viên :" + nv.MaNhanVien);
-        //        nf.Show();
-        //        transfer = 1;
-        //    }
-            
-        //}
+        public void ThemNhanVien(TextBoxX tbhoten, BunifuDropdown ddGioiTinh, TextBoxX tbpass, TextBoxX tbphone, BunifuDatepicker DPNgaySinh, BunifuDropdown ddchucvu, string btavtar, out int transfer)
+        {
+            transfer = 0;
+            HotelObject.NhanVienHo nv = new HotelObject.NhanVienHo();
+            string temp = null;
+            ChuanHoa ch = new ChuanHoa();
+            nv.NgaySinh = DPNgaySinh.Value.ToString();
+            ch.ChuanHoaDate(nv.NgaySinh, out day, out month, out year);
+            nv.NgaySinh = year + month + day;
+            nv.MaNhanVien = conn.LayBien("EXEC PROC_SELECT_MANHANVIEN", 0);
+            for (int i = 2; i < nv.MaNhanVien.Length; i++)
+            {
+                temp = temp + nv.MaNhanVien[i];
+            }
+            nv.MaNhanVien = "NV" + (Int32.Parse(temp) + 1).ToString();
+            nv.HoTen = ch.CH_Name(tbhoten);
+            if (ch.Check_Phone(tbphone) == false)
+            {
+                Notification nf = new Notification("LỖI", "Số điện thoại không hợp lệ", "Mời bạn nhập lại.");
+                nf.Show();
+                transfer = 0;
+            }
+            else if (ch.Check_Text_Name(tbhoten) == false)
+            {
+                Notification nf = new Notification("LỖI", "Tên nhập chứa chữ số", "Mời bạn nhập lại.");
+                nf.Show();
+                transfer = 0;
+            }
+            else
+            {
+                conn.InsertDeleteUpdate("EXEC PROC_INSERT_NHANVIEN '" + nv.MaNhanVien + "','" + tbpass.Text + "',N'" + tbhoten.Text + "',N'" + ddchucvu.selectedValue + "',N'" + ddGioiTinh.selectedValue + "','" + btavtar + "','" + year + month + day + "','" + tbphone.Text + "'");
+                Notification nf = new Notification("THÊM NHÂN VIÊN", "Thêm nhân viên thành công.", "Mã nhân viên :" + nv.MaNhanVien);
+                nf.Show();
+                transfer = 1;
+            }
+
+        }
     }
 }
