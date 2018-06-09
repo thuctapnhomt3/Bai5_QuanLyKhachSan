3681d22aa86d30e07751a98217ad17886a8802ff
Nguyen My <nguyenmyqb97@gmail.com>
2018-06-09 17:22:38 +0700

    Thuc hien chuc nang thanh toan

--- QuanLyKhachSan/QuanLyKhachSan/QuanLyKhachSan/Controller/ThanhToanController.cs
+++ QuanLyKhachSan/QuanLyKhachSan/QuanLyKhachSan/Controller/ThanhToanController.cs
@@ -12,39 +12,39 @@ namespace QuanLyKhachSan.Controller
     {
         Connection conn = new Connection();
         ChuanHoa ch = new ChuanHoa();
-        //public void ThanhToan(Label lbtenphong,Label lbhoten,Label lbloaiphong,Label lbngayvao,Label lbngayra,Label lbthanhtien,Label lbdichvu,Label lbtongtien)
-        //{
-        //    HotelObject.ThanhToanHo tt = new HotelObject.ThanhToanHo();
-        //    HotelObject.ThuePhongHo tp = new HotelObject.ThuePhongHo();
-        //    HotelObject.SuDungDichVuHo sd = new HotelObject.SuDungDichVuHo();
-        //    string year, month, day,ngayvao=null;
-        //    string year1, month1, day1, ngayra = null;
-        //    string yearnow, monthnow, daynow;
-        //    lbhoten.Text = conn.LayBien("EXEC PROC_SELECT_TENKHACHHANG N'" + lbtenphong.Text + "'", 0);
-        //    lbloaiphong.Text = conn.LayBien("EXEC PROC_SELECT_LOAIPHONG N'" + lbtenphong.Text + "'", 0);
-        //    tp.NgayVao = conn.LayBien("EXEC PROC_SELECT_NGAYVAO N'"+lbtenphong.Text+"'",0);
-        //    tp.NgayRa = conn.LayBien("EXEC PROC_SELECT_NGAYRA N'" + lbtenphong.Text + "'", 0);
-        //    tp.MaThue = conn.LayBien("EXEC PROC_SELECT_MATHUE N'" + lbtenphong.Text + "'", 0);
-        //    ch.ChuanHoaDate(tp.NgayRa, out day,out month,out year); 
-        //    ch.ChuanHoaDate(tp.NgayVao, out day1, out month1, out year1);
-        //    ch.ChuanHoaDate(DateTime.Now.ToString(), out daynow, out monthnow, out yearnow);
-        //    lbngayvao.Text = day + " / " + month + " / " + year;
-        //    lbngayra.Text = day1 + " / " + month1 + " / " + year1;
-        //    string giatien = conn.LayBien("EXEC PROC_SELECT_GIATIEN N'" + lbtenphong.Text + "'",0);
-        //    string tongtientt=ch.TongTien(ngayra, ngayvao, Int32.Parse(giatien));
+        public void ThanhToan(Label lbtenphong, Label lbhoten, Label lbloaiphong, Label lbngayvao, Label lbngayra, Label lbthanhtien, Label lbdichvu, Label lbtongtien)
+        {
+            HotelObject.ThanhToanHo tt = new HotelObject.ThanhToanHo();
+            HotelObject.ThuePhongHo tp = new HotelObject.ThuePhongHo();
+            HotelObject.SuDungDichVuHo sd = new HotelObject.SuDungDichVuHo();
+            string year, month, day, ngayvao = null;
+            string year1, month1, day1, ngayra = null;
+            string yearnow, monthnow, daynow;
+            lbhoten.Text = conn.LayBien("EXEC PROC_SELECT_TENKHACHHANG N'" + lbtenphong.Text + "'", 0);
+            lbloaiphong.Text = conn.LayBien("EXEC PROC_SELECT_LOAIPHONG N'" + lbtenphong.Text + "'", 0);
+            tp.NgayVao = conn.LayBien("EXEC PROC_SELECT_NGAYVAO N'" + lbtenphong.Text + "'", 0);
+            tp.NgayRa = conn.LayBien("EXEC PROC_SELECT_NGAYRA N'" + lbtenphong.Text + "'", 0);
+            tp.MaThue = conn.LayBien("EXEC PROC_SELECT_MATHUE N'" + lbtenphong.Text + "'", 0);
+            ch.ChuanHoaDate(tp.NgayRa, out day, out month, out year);
+            ch.ChuanHoaDate(tp.NgayVao, out day1, out month1, out year1);
+            ch.ChuanHoaDate(DateTime.Now.ToString(), out daynow, out monthnow, out yearnow);
+            lbngayvao.Text = day + " / " + month + " / " + year;
+            lbngayra.Text = day1 + " / " + month1 + " / " + year1;
+            string giatien = conn.LayBien("EXEC PROC_SELECT_GIATIEN N'" + lbtenphong.Text + "'", 0);
+            string tongtientt = ch.TongTien(ngayra, ngayvao, Int32.Parse(giatien));
 
-        //    sd.DonGia = conn.LayBien("EXEC PROC_SELECT_DONGIADV N'" + lbtenphong.Text + "'",0);
-        //    if(sd.DonGia == "")
-        //    {
-        //        sd.DonGia = "0";
-        //    }
-        //    lbdichvu.Text= sd.DonGia.ToString();
-            
-        //    TimeSpan time = DateTime.Parse(tp.NgayRa) - DateTime.Parse(tp.NgayVao);
-        //    int dayz = time.Days+1;
-        //    lbthanhtien.Text = (Int32.Parse(tongtientt) *dayz *1000).ToString();
-        //    lbtongtien.Text = (Int32.Parse(sd.DonGia) + (Int32.Parse(tongtientt) * dayz * 1000)).ToString();
-        //    conn.InsertDeleteUpdate("EXEC PROC_INSERT_THANHTOAN '"+tp.MaThue+"','"+giatien+"','"+yearnow+monthnow+daynow+"'");
-        //}
+            sd.DonGia = conn.LayBien("EXEC PROC_SELECT_DONGIADV N'" + lbtenphong.Text + "'", 0);
+            if (sd.DonGia == "")
+            {
+                sd.DonGia = "0";
+            }
+            lbdichvu.Text = sd.DonGia.ToString();
+
+            TimeSpan time = DateTime.Parse(tp.NgayRa) - DateTime.Parse(tp.NgayVao);
+            int dayz = time.Days + 1;
+            lbthanhtien.Text = (Int32.Parse(tongtientt) * dayz * 1000).ToString();
+            lbtongtien.Text = (Int32.Parse(sd.DonGia) + (Int32.Parse(tongtientt) * dayz * 1000)).ToString();
+            conn.InsertDeleteUpdate("EXEC PROC_INSERT_THANHTOAN '" + tp.MaThue + "','" + giatien + "','" + yearnow + monthnow + daynow + "'");
+        }
     }
 }
